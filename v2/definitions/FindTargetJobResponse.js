5baa88551d96daf9917d3e7a4836e9f6f997d8d6
Dominic Bou-Samra <dominic.bou.samra@gmail.com>
2017-12-18 12:06:39 +1100

    fix(find-target): The jobResults field was an array rather than object

--- v2/definitions/FindTargetJobResponse.js
+++ v2/definitions/FindTargetJobResponse.js
@@ -32,80 +32,75 @@ module.exports = {
       description: 'Publicly accessible POST endpoint for receiving job status updates',
     },
     jobResults: {
-      type: 'array',
-      items: {
-        type: 'object',
-        required: [
-          'target',
-          'images',
-          'hitl',
-        ],
-        properties: {
-          target: {
-            type: 'object',
-            required: [
-              'class',
-              'images',
-            ],
-            properties: {
-              'class': {
+      type: 'object',
+      required: [
+        'target',
+        'images',
+        'hitl',
+      ],
+      properties: {
+        target: {
+          type: 'object',
+          required: [
+            'class',
+            'images',
+          ],
+          properties: {
+            'class': {
+              type: 'string',
+              description: 'The class (target) found in the image',
+            },
+            customId: {
+              type: 'string',
+              description: 'An arbitrary client specific resource identifier to reference this target image (usually UUID)',
+            },
+            images: {
+              type: 'array',
+              description: 'A list of target image URLs that were passed in during the request)',
+              items: {
                 type: 'string',
-                description: 'The class (target) found in the image',
+                description: 'URL of target image',
               },
-              customId: {
-                type: 'string',
-                description: 'An arbitrary client specific resource identifier to reference this target image (usually UUID)',
-              },
-              images: {
-                type: 'array',
-                description: 'A list of target image URLs that were passed in during the request)',
-                items: {
-                  type: 'string',
-                  description: 'URL of target image',
-                },
-                maxItems: 3,
-                minItems: 1,
-              },
+              maxItems: 3,
+              minItems: 1,
             },
           },
-          images: {
-            type: 'array',
-            items: {
-              $ref: '#/definitions/ImageResponseItem',
-            },
-            minItems: 1,
+        },
+        images: {
+          type: 'array',
+          items: {
+            $ref: '#/definitions/ImageResponseItem',
           },
-          hitl: {
-            type: 'boolean',
-            description: 'Whether human-in-the-loop verification was used for this class',
-          },
+          minItems: 1,
+        },
+        hitl: {
+          type: 'boolean',
+          description: 'Whether human-in-the-loop verification was used for this class',
         },
       },
     },
   },
   example: {
     id: '5d689c71-e68f-46c3-ac71-4053806e71de',
-    jobResults: [
-      {
-        target: {
-          'class': 'person',
-          customId: 'custom_target_id',
-          images: [
-            'https://publicly.available.domain.net/person-001.jpg',
-            'https://publicly.available.domain.net/person-002.jpg',
-            'https://publicly.available.domain.net/person-003.jpg',
-          ],
-        },
+    jobResults: {
+      target: {
+        'class': 'person',
+        customId: 'custom_target_id',
         images: [
-          {
-            url: 'https://publicly.available.domain.net/image-xxx.jpg',
-            customId: 'some_person_filled_image',
-            proxyUrl: 'https://api.quickpix.io/images/c7e9a2ca-c2ee-4290-90b4-04fe3df35be0',
-          }
+          'https://publicly.available.domain.net/person-001.jpg',
+          'https://publicly.available.domain.net/person-002.jpg',
+          'https://publicly.available.domain.net/person-003.jpg',
         ],
-        hitl: true,
       },
-    ],
+      images: [
+        {
+          url: 'https://publicly.available.domain.net/image-xxx.jpg',
+          customId: 'some_person_filled_image',
+          proxyUrl: 'https://api.quickpix.io/images/c7e9a2ca-c2ee-4290-90b4-04fe3df35be0',
+        }
+      ],
+      hitl: true,
+    },
     createdAt: 1487648348,
     status: 'COMPLETED_SUCCESSFULLY',
     customId: '6c78df0a-67b6-4d5f-93cf-5820cfee501c',
