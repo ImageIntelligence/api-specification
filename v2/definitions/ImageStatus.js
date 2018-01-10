d32c40a7dcaf2cd7cdf356bd384dfea79024eb4b
Dominic Bou-Samra <dominic.bou.samra@gmail.com>
2018-01-11 09:55:10 +1100

    feat(api-v2): ResultStatus changes

--- v2/definitions/ImageStatus.js
+++ v2/definitions/ImageStatus.js
@@ -1,11 +1,16 @@
 module.exports = {
   type: 'string',
   description: 'The processing status for an image',
-  enum: [
+  enum: [    
     'COMPLETED_SUCCESSFULLY',
+    'IN_PROGRESS',
+    'UNKNOWN_ERROR',
+    'JOB_ERROR',
     'IMAGE_DOWNLOAD_ERROR',
     'INVALID_IMAGE_FORMAT_ERROR',
-    'UNKNOWN_ERROR',
-    'IN_PROGRESS',
+    'SPIDERWEBS_ERROR',
+    'DETECTION_ENGINE_ERROR',
+    'MODEL_SERVICE_ERROR',
+    'MALFORMED_MESSAGE_ERROR'
   ],
 };
