789caf5d6fd1bcba0ec2517030be15417590f42e
David Vuong <david.vuong256@gmail.com>
2018-09-28 12:15:12 +1000

    feat(face-rec): add face-rec/recognize and refactor defs

--- v2/Api.js
+++ v2/Api.js
@@ -3,7 +3,7 @@ module.exports = {
   host: 'api.imageintelligence.com',
   basePath: '/v2',
   info: {
-    version: require('../package.json').version,
+    version: require('../package.json').version || '2',
     title: 'Image Intelligence API Documentation',
   },
   schemes: [
@@ -33,25 +33,43 @@ module.exports = {
     '/ask/{id}': require('./paths/AskGetById'),
     '/feedback': require('./paths/Feedback'),
     '/feedback/{id}': require('./paths/FeedbackGetById'),
+    '/face-recognition/recognize': require('./paths/faceRecognition/RecognizeCreate'),
   },
   definitions: {
-    ClientCredentials: require('./definitions/ClientCredentials'),
-    AccessTokenResponse: require('./definitions/AccessTokenResponse'),
-    ImageStatus: require('./definitions/ImageStatus'),
-    JobStatus: require('./definitions/JobStatus'),
+    // Common //
+    BoundingBox: require('./definitions/common/BoundingBox'),
+    ImageStatus: require('./definitions/common/ImageStatus'),
+    JobStatus: require('./definitions/common/JobStatus'),
+    ImageRequestItem: require('./definitions/common/ImageRequestItem'),
+    ImageResponseItem: require('./definitions/common/ImageResponseItem'),
 
-    ImageRequestItem: require('./definitions/ImageRequestItem'),
-    ImageResponseItem: require('./definitions/ImageResponseItem'),
+    // Authentication //
+
+    ClientCredentials: require('./definitions/authentication/ClientCredentials'),
+    AccessTokenResponse: require('./definitions/authentication/AccessTokenResponse'),
+
+    // Detect //
 
     DetectJobRequest: require('./definitions/DetectJobRequest'),
     DetectJobResponse: require('./definitions/DetectJobResponse'),
     DetectJobResponseItem: require('./definitions/DetectJobResponseItem'),
 
+    // Match //
+
     MatchJobRequest: require('./definitions/MatchJobRequest'),
     MatchJobResponse: require('./definitions/MatchJobResponse'),
 
+    // Ask //
+
     AskJobRequest: require('./definitions/AskJobRequest'),
     AskJobResponse: require('./definitions/AskJobResponse'),
+
+    // FaceRecognition //
+
+    FaceRecognitionCreateJobRequest: require('./definitions/faceRecognition/CreateJobRequest'),
+    FaceRecognitionCreateJobResponse: require('./definitions/faceRecognition/CreateJobResponse'),
+
+    // Feedback //
 
     FeedbackRequest: require('./definitions/FeedbackRequest'),
     FeedbackRequestClassItem: require('./definitions/FeedbackRequestClassItem'),
---
+++ v2/definitions/faceRecognition/CreateJobRequest.js
@@ -0,0 +1,52 @@
+module.exports = {
+  type: 'object',
+  required: [
+    'images',
+    'groupId',
+  ],
+  properties: {
+    images: {
+      type: 'array',
+      description: 'A list of images. Each object should contain either the image URL or blob of image bytes',
+      items: {
+        $ref: '#/definitions/ImageRequestItem',
+      },
+      maxItems: 64,
+      minItems: 1,
+    },
+    groupId: {
+      type: 'string',
+      description: 'A groupId generated after creating a group (UUID)',
+      maxLength: 64,
+    },
+    webhookUrl: {
+      type: 'string',
+      description: 'Publicly accessible POST endpoint for receiving job status updates',
+      maxLength: 512,
+    },
+    customId: {
+      type: 'string',
+      description: 'An arbitrary client specific resource identifier to reference this job (usually UUID)',
+      maxLength: 64,
+    },
+    feedId: {
+      type: 'string',
+      description: 'User specified ID to reference the source of the images within this job',
+      maxLength: 64,
+    },
+  },
+  example: {
+    images: [
+      {
+        url: 'https://publicly.available.domain.net/image-001.jpg',
+      },
+      {
+        url: 'https://publicly.available.domain.net/image-002.jpg',
+      },
+    ],
+    groupId: 'd4ef671e-2067-446e-8659-1dcd32b09c9a',
+    customId: '6c78df0a-67b6-4d5f-93cf-5820cfee501c',
+    feedId: 'Camera_123456',
+    webhookUrl: 'https://publicly.available.domain.net/90f8754aa4e287bbb2c07ec99ade0de72ddbd4b6Et',
+  },
+};
