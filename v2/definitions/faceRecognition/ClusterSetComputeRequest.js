84e13b579953e93d69d3311ca2baab5e6fef5e74
David Vuong <david.vuong256@gmail.com>
2018-10-15 13:26:33 +1100

    feat(ClusterSetComputeRequest): flatten minImageSize obj

--- v2/definitions/faceRecognition/ClusterSetComputeRequest.js
+++ v2/definitions/faceRecognition/ClusterSetComputeRequest.js
@@ -7,31 +7,20 @@ module.exports = {
     groupId: {
       type: 'string',
     },
-    minImageSize: {
-      type: 'object',
-      required: [
-        'height',
-        'width',
-      ],
-      properties: {
-        height: {
-          type: 'number',
-          format: 'int32',
-          default: 100,
-        },
-        width: {
-          type: 'number',
-          format: 'int32',
-          default: 100,
-        },
-      },
+    minWidth: {
+      type: 'number',
+      format: 'int32',
+      default: 64,
+    },
+    minHeight: {
+      type: 'number',
+      format: 'int32',
+      default: 64,
     },
   },
   example: {
     groupId: '633091ed-4c05-459b-b9dc-061c66591f92',
-    minImageSize: {
-      width: 128,
-      height: 128,
-    },
+    minWidth: 32,
+    minHeight: 32,
   },
 };
