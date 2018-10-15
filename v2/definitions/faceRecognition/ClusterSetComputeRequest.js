4ae744b3fab923fdca8d4e9427e472eeeeeb2a48
David Vuong <david.vuong256@gmail.com>
2018-10-15 11:47:50 +1100

    feat(ClusterSetComputeRequest): add minImageSize

--- v2/definitions/faceRecognition/ClusterSetComputeRequest.js
+++ v2/definitions/faceRecognition/ClusterSetComputeRequest.js
@@ -7,8 +7,31 @@ module.exports = {
     groupId: {
       type: 'string',
     },
+    minImageSize: {
+      type: 'object',
+      required: [
+        'height',
+        'width',
+      ],
+      properties: {
+        height: {
+          type: 'number',
+          format: 'int32',
+          default: 100,
+        },
+        width: {
+          type: 'number',
+          format: 'int32',
+          default: 100,
+        },
+      },
+    },
   },
   example: {
     groupId: '633091ed-4c05-459b-b9dc-061c66591f92',
+    minImageSize: {
+      width: 128,
+      height: 128,
+    },
   },
 };
