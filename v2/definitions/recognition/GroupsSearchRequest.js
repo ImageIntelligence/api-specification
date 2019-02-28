9fc1c4859c0fe174ecdfaf05d3d32ffadc90fcda
Jason Shin <visualbbasic@gmail.com>
2019-02-28 16:51:48 +1100

    docs(GroupsSearchRequest): adding min, max, default

--- v2/definitions/recognition/GroupsSearchRequest.js
+++ v2/definitions/recognition/GroupsSearchRequest.js
@@ -1,18 +1,20 @@
 module.exports = {
   type: 'object',
-  required: [
-    'limit',
-    'offset',
-    'sortBy',
-  ],
+  required: [],
   properties: {
     limit: {
       type: 'number',
       description: 'Number of maximum objects in the query',
+      minimum: 1,
+      maximum: 100,
+      default: 20,
     },
     offset: {
       type: 'number',
       description: 'Number of results to skip before returning a result',
+      minimum: 0,
+      maximum: 100,
+      default: 0,
     },
     start: {
       type: 'number',
