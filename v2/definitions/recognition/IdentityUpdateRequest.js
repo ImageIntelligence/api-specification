dbc2409385acd54706434d8368dcc3f2fc75e9a7
Jason Shin <visualbbasic@gmail.com>
2019-03-04 15:14:19 +1100

    docs(faceIds): updating faceId to imageId

--- v2/definitions/recognition/IdentityUpdateRequest.js
+++ v2/definitions/recognition/IdentityUpdateRequest.js
@@ -1,15 +1,15 @@
 module.exports = {
   type: 'object',
-  required: ['faceIdsToAdd', 'faceIdsToRemove'],
+  required: ['imageIdsToAdd', 'imageIdsToRemove'],
   properties: {
-    faceIdsToAdd: {
+    imageIdsToAdd: {
       type: 'array',
       items: {
         type: 'string',
         minItems: 1
       }
     },
-    faceIdsToRemove: {
+    imageIdsToRemove: {
       type: 'array',
       items: {
         type: 'string',
@@ -18,11 +18,11 @@ module.exports = {
     }
   },
   example: {
-    faceIdsToAdd: [
+    imageIdsToAdd: [
       'af278ded-5412-4916-bb5c-13769bfb7644',
       '633081ed-4c05-459b-bddc-061c66581f92',
       'c3c61384-1861-4026-be2e-4d70646231c0'
     ],
-    faceIdsToRemove: ['57ff6229-9082-4e14-aff6-4244e9dad9eb', '9521422c-76d0-4f38-aa10-1c12d11b93f5']
+    imageIdsToRemove: ['57ff6229-9082-4e14-aff6-4244e9dad9eb', '9521422c-76d0-4f38-aa10-1c12d11b93f5']
   }
 };
