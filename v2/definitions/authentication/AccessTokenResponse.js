3f76269a7199fbd83b7d8e4d8ebbac96b26ca7a0
David Vuong <david.vuong256@gmail.com>
2019-03-11 10:58:15 +1100

    feat(AccessTokenRespnose): update sample scopes

--- v2/definitions/authentication/AccessTokenResponse.js
+++ v2/definitions/authentication/AccessTokenResponse.js
@@ -1,40 +1,33 @@
 module.exports = {
   type: 'object',
-  required: [
-    'accessToken',
-    'expiresAt',
-    'issuedAt',
-    'orgName',
-    'appName',
-    'scope',
-  ],
+  required: ['accessToken', 'expiresAt', 'issuedAt', 'orgName', 'appName', 'scope'],
   properties: {
     accessToken: {
       type: 'string',
-      description: 'A generated access token used to authenticate with the API',
+      description: 'A generated access token used to authenticate with the API'
     },
     expiresAt: {
       type: 'number',
       format: 'int64',
-      description: 'The UNIX timestamp for when this access token expires',
+      description: 'The UNIX timestamp for when this access token expires'
     },
     issuedAt: {
       type: 'number',
       format: 'int64',
-      description: 'The UNIX timestamp for when this access token was generated',
+      description: 'The UNIX timestamp for when this access token was generated'
     },
     orgName: {
       type: 'string',
-      description: 'Name of the organization this token belongs to',
+      description: 'Name of the organization this token belongs to'
     },
     appName: {
       type: 'string',
-      description: 'Name of the application this access token was generated from',
+      description: 'Name of the application this access token was generated from'
     },
     scope: {
       type: 'string',
-      description: 'A list of space delimited scopes',
-    },
+      description: 'A list of space delimited scopes'
+    }
   },
   example: {
     accessToken: 'eyJzdHQiOiJhY2....xUC3RJFY1T8',
@@ -42,6 +35,7 @@ module.exports = {
     issuedAt: 1489019052385,
     orgName: 'acme',
     appName: 'acme-app',
-    scope: 'find-target:create find-target:read find-object:create find-object:read',
-  },
+    scope:
+      'recognition:read recognition:create recognition:update recognition:remove ask:read ask:create match:read match:create feedback:create feedback:read detect:create detect:read find-target:create find-target:read find-object:create find-object:read'
+  }
 };
